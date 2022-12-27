2e43f70b4b4de04bc79f7b1ac744f1a504a9959a
Zachri Alrashid <zachrialrashid@gmail.com>
2022-12-28 06:43:33 +0700

    "Contact" section
    1. Apply new layout & content for contact form; new validation response for form fields (Name, Email, Message), enable submit button, pass submission data to GSheet, delete template response to be replaced with custom modal window, and delete background image asset (map-image.png)
    2. Update navbar menus into 4 (Exclude "Snapshot"
    3. Move "Clients" section (Tools) above "Snapshot"
    4. Minor copy changes; replace "Experience" with "Profile" and replace masthead button with "Hello"

--- js/custom.js
+++ js/custom.js
@@ -5,17 +5,62 @@
 */
 /*!
  * Custom JS by Zachri
- * Additional function for snapshot items
  */
-//
-// Scripts
-// 
 
+
+// Activate SimpleLightbox plugin for snapshot items
 window.addEventListener('DOMContentLoaded', event => {
 
-    // Activate SimpleLightbox plugin for snapshot items
     new SimpleLightbox({
         elements: '#snapshot a.snapshot-box'
     });
 
 });
+
+// Pass form submissions to GSheet
+window.addEventListener("load", function () {
+    const form = document.querySelector("form");
+    
+    form.addEventListener("submit", function (event) {
+      event.preventDefault();
+      sendData();
+    });
+    function sendData() {
+      const FD = new FormData(form);
+
+      let url = form.dataset.gfUrl;
+      for(let [key, value] of FD) {
+        url = url.replace(key, value);
+      }
+      let opts = {
+          method: "POST",
+          mode: "no-cors",
+          redirect: "follow", 
+          referrer: "no-referrer"
+      }
+        return fetch(url, opts);
+    
+    }
+
+});
+
+// Disable form submissions if there are invalid fields
+(function () {
+    'use strict'
+
+  // Fetch all the forms to apply custom Bootstrap validation styles
+  var forms = document.querySelectorAll('.needs-validation')
+
+  // Loop over them and prevent submission
+  Array.prototype.slice.call(forms)
+    .forEach(function (form) {
+      form.addEventListener('submit', function (event) {
+        if (!form.checkValidity()) {
+          event.preventDefault()
+          event.stopPropagation()
+        }
+        form.classList.add('was-validated')
+      }, false)
+    })
+
+})()
